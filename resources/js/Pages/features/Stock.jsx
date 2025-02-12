b7356713ab73a82556989cb5afdda17a077f355f
MuhammadMardiyasah <mhmmardi22@gmail.com>
2025-02-12 17:15:37 +0700

    add folder features and move file

---
+++ resources/js/Pages/features/Stock.jsx
@@ -0,0 +1,35 @@
+import DashboardAdminLayout from '@/Layouts/DashboardAdminLayout';
+import DashboardInventoryLayout from '@/Layouts/DashboardInventoryLayout';
+import DataTableStock from "@/Components/DataTableStock";
+import { Head } from '@inertiajs/react';
+import React from 'react';
+
+export default function Stock({auth, mustVerifyEmail, status, title, inbound}) {
+    const getLayout = (role) => {
+        switch (role) {
+            case 'wrhs':
+                return DashboardInventoryLayout; 
+            default:
+                return DashboardAdminLayout;
+        }
+    };
+
+    const Layout = getLayout(auth.user.role);
+    const data = inbound;
+
+    return (
+        <Layout auth={auth}>
+            <Head title="Dashboard" />
+            <div className="fixed bg-[#f5f5f5] w-full h-18 md:h-14 px-6 py-4 border-b z-10">
+                <h1 className="text-xl font-bold">{title}</h1>
+            </div>
+            <div className="p-6 mt-14">
+              <DataTableStock mustVerifyEmail={mustVerifyEmail} status={status} data={data} />
+          </div>
+
+        </Layout>
+    );
+}
+
+
+
