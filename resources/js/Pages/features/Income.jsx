575db51757ca13901da195b0487409745168be89
MuhammadMardiyasah <mhmmardi22@gmail.com>
2025-02-13 03:07:49 +0700

    add features budgetcontrol, income, outcome

---
+++ resources/js/Pages/features/Income.jsx
@@ -0,0 +1,33 @@
+import DashboardAdminLayout from "@/Layouts/DashboardAdminLayout";
+import DashboardFinanceLayout from "@/Layouts/DashboardFinanceLayout"; 
+import DataTableIncome from "@/Components/DataTableIncome";
+import { Head } from '@inertiajs/react';
+import React from 'react';
+
+export default function Shipment({auth, mustVerifyEmail, status, title}) {
+    const getLayout = (role) => {
+        switch (role) {
+            case 'fnc':
+                return DashboardFinanceLayout; 
+            default:
+                return DashboardAdminLayout;
+        }
+    };
+
+    const Layout = getLayout(auth.user.role);
+    const data = [];
+
+    return (
+        <Layout auth={auth}>
+            <Head title="Dashboard" />
+            <div className="fixed bg-[#f5f5f5] w-full h-18 md:h-14 px-6 py-4 border-b z-10">
+                <h1 className="text-xl font-bold">{title}</h1>
+               
+            </div>
+            <div className="p-6 mt-14">
+              <DataTableIncome mustVerifyEmail={mustVerifyEmail} status={status} data={data} />
+          </div>
+
+        </Layout>
+    );
+}
